b544a1ac058ab0960a6962ac899fb75bab44bcc8
IHyunSu <zmfnwj119@gmail.com>
2025-05-02 19:59:06 +0900

    feat : login and logout

--- src/main/java/org/hyunsu/controller/UserController.java
+++ src/main/java/org/hyunsu/controller/UserController.java
@@ -6,7 +6,7 @@ import org.hyunsu.dto.UserSimpleResponseDto;
 import org.hyunsu.service.UserService;
 //패키지명이 다를 시 본인 패키지명으로 작성해야 오류가 안납니다.
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired; //생성자를 통한 주입
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,17 +19,5 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
-    }
-
-    @PostMapping("/auth/register")
-    public ResponseEntity<UserSimpleResponseDto> registerUser(@RequestBody UserRegistrationRequestDto userRegistrationRequestDto) {
-        User user = new User(
-                userRegistrationRequestDto.getUsername(),
-                userRegistrationRequestDto.getPassword(),
-                userRegistrationRequestDto.getName()
-        );
-        UserSimpleResponseDto savedUser = userService.saveUser(user);
-
-        return ResponseEntity.ok(savedUser);
     }
 }
---
+++ src/main/java/org/hyunsu/sevice/AuthService.java
@@ -0,0 +1,51 @@
+package org.hyunsu.sevice;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.hyunsu.domain.User;
+import org.hyunsu.dto.UserSimpleResponseDto;
+import org.hyunsu.dto.request.LoginRequestDto;
+import org.hyunsu.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    private final org.hyunsu.service.UserService userService;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AuthService(org.hyunsu.service.UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
+    }
+
+    public UserSimpleResponseDto login(LoginRequestDto loginRequestDto, HttpServletRequest request) {
+        User user = userRepository.findByUsername(loginRequestDto.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        if (!loginRequestDto.getPassword().equals(loginRequestDto.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 잘못됐습니다.");
+        }
+
+        HttpSession session = request.getSession(); // session이 존재하지 않으면 새로운 세션 생성
+        session.setAttribute("user", user);
+
+        return userService.convertUserToSimpleDto(user, user);
+    }
+
+    public void logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false); // false를 전달하면, 현재 session이 존재하지 않으면 null을 반환
+        if (session != null) {
+            session.invalidate();  // 세션 비활성화
+        }
+    }
+
+    public User getCurrentUser(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            throw new IllegalStateException("로그인되지 않았습니다.");  // unAuthorized
+        }
+        return (User) session.getAttribute("user");
+    }
+}
